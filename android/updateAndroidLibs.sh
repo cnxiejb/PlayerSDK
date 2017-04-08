31ef3ea9025888da50ce8b0b4e7c1bfd5befb0eb
xiejinbao <xiejinbao@corp.netease.com>
2017-04-08 15:42:52 +0800

    更新编译脚本

--- android/updateAndroidLibs.sh
+++ android/updateAndroidLibs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-rm -rf ijklibs
+rm -rf ijklibs.zip
 mkdir ijklibs
 
+sh ./compile-ijk.sh all
 mv ./ijkplayer/ijkplayer-x86/src/main/libs/x86 ./ijklibs/
 mv ./ijkplayer/ijkplayer-x86_64/src/main/libs/x86_64 ./ijklibs/
 mv ./ijkplayer/ijkplayer-arm64/src/main/libs/arm64-v8a ./ijklibs/
 mv ./ijkplayer/ijkplayer-armv5/src/main/libs/armeabi   ./ijklibs/
 mv ./ijkplayer/ijkplayer-armv7a/src/main/libs/armeabi-v7a ./ijklibs/
+
+zip -r  ijklibs.zip ijklibs
+rm -rf ijklibs
---
+++ updateMintPlayerSDK.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sh ios/updateiOSframework.sh
+sh android/updateAndroidLibs.sh
